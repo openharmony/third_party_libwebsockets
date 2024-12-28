15fb074d201e5ded7a33e5e9bc14253673a530a2
cy <chenyan209@h-partners.com>
2024-12-28 06:58:16 +0000

    update include/lws_config_private.h.
    Signed-off-by: cy <chenyan209@h-partners.com>

--- include/lws_config_private.h
+++ include/lws_config_private.h
@@ -8,8 +8,7 @@
  * deal in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
  * sell copies of the Software, and to permit persons to whom the Software is
- * furnishe
- * d to do so, subject to the following conditions:
+ * furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
